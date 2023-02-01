1464a505a9062b6a7ff1340d5f62ac73e45d5237
Dongho Seo <ehcws333@gmail.com>
2023-02-01 22:55:00 +0900

    :wrench: testing modified env variable

--- config/index.ts
+++ config/index.ts
@@ -3,11 +3,17 @@ export const ARTICLE_DB = process.env.NOTION_ARTICLE_DATABASE_ID;
 export const rootNotionPageId = 'a86df9cb1ec744b7a2b8a250811734fa';
 
 const cntEnv = process.env.NEXT_PUBLIC_MODE;
+console.log(cntEnv);
 
-const isProduction = () => {
-  return cntEnv === 'production';
+const environment = () => {
+  switch (cntEnv) {
+    case 'production':
+      return 'https://sleeepy-blog.vercel.app';
+    case 'development':
+      return 'http://localhost:3000';
+    default:
+      return 'http://localhost:3000';
+  }
 };
 
-export const server = isProduction()
-  ? 'https://sleeepy-blog.vercel.app'
-  : 'http://localhost:3000';
+export const server = environment();
