1eb46de7ee578ba4abe69b8a7995f7de5a5885a6
Dongho Seo <ehcws333@gmail.com>
2023-02-02 11:29:59 +0900

    :wrench: agaaain

--- config/index.ts
+++ config/index.ts
@@ -1,19 +1,8 @@
+import * as process from 'process';
 export const NOTION_TOKEN = process.env.NOTION_TOKEN;
 export const ARTICLE_DB = process.env.NOTION_ARTICLE_DATABASE_ID;
 export const rootNotionPageId = 'a86df9cb1ec744b7a2b8a250811734fa';
-
-const cntEnv = process.env.NEXT_PUBLIC_MODE;
-console.log(cntEnv);
-
-const environment = () => {
-  switch (cntEnv) {
-    case 'production':
-      return 'https://notion-sleeep23-blog.vercel.app';
-    case 'development':
-      return 'http://localhost:3000';
-    default:
-      return 'https://notion-sleeep23-blog.vercel.app';
-  }
-};
-
-export const server = environment();
+export const server =
+  process.env.NEXT_PUBLIC_MODE === 'production'
+    ? 'https://sleeepy-blog.vercel.app'
+    : 'http://localhost:3000';
