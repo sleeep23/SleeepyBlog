cd9058bf3c3f4641c4ca97e7bd0b420d4f2b66a0
Dongho Seo <ehcws333@gmail.com>
2023-02-09 23:24:36 +0900

    :poop: find the cause of delay

--- lib/type.ts
+++ lib/type.ts
@@ -1,5 +1,3 @@
-import { MdBlock } from 'notion-to-md/build/types';
-
 export interface DbPostContents {
   name: string;
   posts: Array<PostThumbnailType>;
@@ -24,5 +22,4 @@ export interface TagType {
 
 export interface PostContentType extends PostThumbnailType {
   content: string;
-  // content: MdBlock[];
 }
